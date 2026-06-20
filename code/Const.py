7fd8a957f822ff69bea3fbf8bc156e19d3d8ae73
OmettoGustavo <gus_lemao@hotmail.com>
2026-06-20 09:29:08 -0300

    Level 2 and 3 implementation

--- code/Const.py
+++ code/Const.py
@@ -9,11 +9,22 @@ COLOR_BLUE = (0, 0, 255)
 
 # E
 EVENT_POINT = pygame.USEREVENT + 1
+EVENT_TIMEOUT = pygame.USEREVENT + 2
 ENTITY_SPEED = {
     'Level1Bg0': 0,
     'Level1Bg1': 1,
     'Level1Bg2': 2,
     'Level1Bg3': 3,
+    'Level2Bg0': 0,
+    'Level2Bg1': 1,
+    'Level3Bg0': 0,
+    'Level3Bg1': 1,
+    'Level3Bg2': 2,
+    'Level3Bg3': 3,
+    'Level3Bg4': 4,
+    'Level3Bg5': 5,
+    'Level3Bg6': 6,
+    'Level3Bg7': 7,
     'Player1': 3,
     'Player2': 3,
     'Point': 2,
@@ -24,6 +35,16 @@ ENTITY_HEALTH = {
     'Level1Bg1': 999,
     'Level1Bg2': 999,
     'Level1Bg3': 999,
+    'Level2Bg0': 999,
+    'Level2Bg1': 999,
+    'Level3Bg0': 999,
+    'Level3Bg1': 999,
+    'Level3Bg2': 999,
+    'Level3Bg3': 999,
+    'Level3Bg4': 999,
+    'Level3Bg5': 999,
+    'Level3Bg6': 999,
+    'Level3Bg7': 999,
     'Player1': 300,
     'Player2': 300,
     'Point': 50,
@@ -59,7 +80,11 @@ PLAYER_KEY_RIGHT = {
 }
 
 #S
-SPAWN_TIME = 500
+SPAWN_TIME = 550
+
+# T
+TIMEOUT_STEP = 1000 # 1s
+TIMEOUT_LEVEL = 20000 #20s
 
 # W
 WIN_WIDTH = 576
--- code/Game.py
+++ code/Game.py
@@ -9,7 +9,6 @@ from code.Menu import Menu
 
 
 class Game:
-
     def __init__(self):
         pygame.init()
         self.window = pygame.display.set_mode(size=(WIN_WIDTH, WIN_HEIGHT))
@@ -19,11 +18,23 @@ class Game:
             menu = Menu(self.window)
             menu_return = menu.run()
 
+            # If the user chose a game option (Single, Co-op, etc.)
             if menu_return in [MENU_OPTION[0], MENU_OPTION[1], MENU_OPTION[2]]:
-                level = Level(self.window, 'Level1', menu_return)
-                level.run()  
+                player_score = [0, 0] # Player1, Player2
+
+                # List of levels to go through
+                level_names = ['Level1', 'Level2', 'Level3']
+
+                for name in level_names:
+                    level = Level(self.window, name, menu_return, player_score)
+                    level_finished = level.run(player_score)
+
+                    # If the level did not end successfully (e.g., the player died), stop the loop.
+                    if not level_finished:
+                        break
+
+            # If the user chose to exit
             elif menu_return == MENU_OPTION[4]:
                 pygame.quit()
                 sys.exit()
-
 
